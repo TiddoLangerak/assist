91c1523ee6f2d3c874e2166cc51f1392af2ba8e4
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-06 10:01:08 -0300

    WIP parsing

--- index2.ts
+++ index2.ts
@@ -52,7 +52,53 @@ function fileSystemCommand(operation: FileSystemOperation, selector: FileSystemS
     execute: () => operation(selector)
   };
 }
+// For naming, what I need to think about is this:
+// - Every "thing" has multiple representations:
+//  - A function that actually performs the operation
+//  - An object that represents the (part of) operation, which can be folded into the function
+//
+//
+//
+// Do I need both?
+// Rename files recursively. Remove prefix matching /\d+\./.
+// When reading this word for word:
+// Rename -> operation = rename
+// files -> selectorFunc = listFiles
+// recursively -> selector = listFiles(., true)
+// Remove
 
+
+/*
+match(`rename ${fileTarget}. ${renameOp}`, ({ fileTarget, renameOp } : { fileTarget: FileSystemSelector, renameOp : MapFn<Path, Path> } ) => {
+
+});
+*/
+
+function hi(strings:TemplateStringsArray, name:string) {     // hi is tag function
+  return 4;
+}
+const x : string = hi`foo ${"bar"}`;
+
+
+interface Matcher<ARGS extends any[]> {
+  args: ARGS;
+}
+
+function t<ARGS extends any[]>(strings: TemplateStringsArray, ...args: ARGS) : Matcher<ARGS> {
+  return {
+    args
+  };
+}
+
+function m<ARGS extends any[], RESULT>(matcher: Matcher<ARGS>, parser: (...args: ARGS) => RESULT): RESULT {
+  return parser(matcher.args);
+}
+
+//correct
+m(t`foo ${3} bar ${'baz'}`, (x: number, y: string) => "foo");
+//incorrect
+m(t`foo ${'wut'} bar ${'baz'}`, (x: number, y: string) => "foo");
+// TODO: m shouldn't take any as parameters, but should have a structure that allows both matching and extracting the result
 
 
 
