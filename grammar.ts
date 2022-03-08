af240074c5b6c3d7f68332c0a6b5493376780e05
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-07 23:41:00 -0300

    [UNTESTED] grammer for adding bak suffix

--- grammar.ts
+++ grammar.ts
@@ -1,11 +1,34 @@
-import { parse, $ } from './parser';
+import { parse, $, Parser } from './parser';
 import * as ops from './operations';
+import { Operation } from './operations';
 import { FileSystemSelector } from './safefs';
+import * as safefs from './safefs';
 import { MapFn } from './function';
-import { Path } from './path';
+import { path, Path } from './path';
+import { cwd } from 'process';
 
+// TODO: probably move this to safefs
+async function* listFilePaths() {
+  const files = await safefs.listFiles(path(cwd()), false);
+  for await (let file of files) {
+    yield file.path;
+  }
+}
 
-const rename = parse($`rename ${target} ${renameOp}`, (target: FileSystemSelector, renameOp: MapFn<Path, Path>) => {
-  return () => ops.rename(renameOp)(target)
-});
+// TODO: support more targets
+const target : Parser<FileSystemSelector> = 
+  parse($`files`, () => listFilePaths); // TODO: actually filter out files only.
+
+// TODO: make generic. Parse open ended input
+const addBakSuffix: MapFn<Path, Path> = input => path(`${input.fullPath}.bak`);
+const renameOp : Parser<MapFn<Path, Path>> =
+  parse($`add suffix .bak`, () => addBakSuffix);
+
+const rename : Parser<Operation> = 
+  parse(
+    $`rename ${target} ${renameOp}`,
+    (target: FileSystemSelector, renameOp: MapFn<Path, Path>) => {
+      return () => ops.rename(renameOp)(target)
+    }
+  );
 
