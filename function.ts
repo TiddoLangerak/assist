81703fa8d0983366551c642eaae0829ae946880d
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-09 10:35:22 -0300

    Some more misc cleanup

--- function.ts
+++ function.ts
@@ -1,61 +1,71 @@
 export type MapFn<In, Out> = (input: In) => Out;
 
-// TODO: maybe unwrap MapFn again for better errors?
-type ValidateComposeHead2<I, O1, O2, REST extends any[]> = [
-  h1: MapFn<I, O1>,
-  h2: MapFn<O1, O2>
+// A tuple with a valid compose head, i.e. where the output of the first function is accepted as input of the second function
+type ComposeTuple2<I, O1, O2, REST extends any[]> = [
+  h1: (i: I) => O1,
+  h2: (i: O1) => O2,
   ...rest: REST
 ];
 
+type ComposeTuple1<I, O, REST extends any[]> = [
+  h: (i: I) => O,
+  ...rest: REST
+];
+
+// TODO: this currently triggers infinite recursion...
 type ValidateComposeTail<T extends any[], I, O1, O2, REST extends any[]> = (
-        [
-          h: MapFn<O1, O2>,
-          ...rest: REST
-        ] extends ComposeArgs<[MapFn<O1, O2>, ...REST]> 
-          // If it is, all good!
-          ? T 
-          // If recursive fails, then we propagate the "Error" type further (see below)
-          // TODO: this currently duplicates the ComposeArgs clause, we can maybe DRY this up
-          : [
-            h: MapFn<I, O1>,
-            ...rest: ComposeArgs<[MapFn<O1, O2>, ...REST]>
-          ]
+  ComposeTuple1<O1, O2, REST> extends ValidComposeArgs<ComposeTuple1<O1, O2, REST>> 
+    // If it is, all good!
+    ? T 
+    // If recursive fails, then we propagate the "Error" type further (see below)
+    // TODO: this currently duplicates the ValidComposeArgs clause, we can maybe DRY this up
+    : [
+      h: MapFn<I, O1>,
+      ...rest: ValidComposeArgs<[MapFn<O1, O2>, ...REST]>
+    ]
 );
 
-type ComposeError<T extends any[]> =( // Error case
-         // If the first 2 don't match, then we construct a type that will fail on the second parameter (i.e. a type that forces a match), and any[] for the remainder
-         // We have some extra conditional here to capture the parameters again (given that the previous conditional was NOT a match)
-        T extends [
-          h: MapFn<infer I, infer O1>,
-          ...rest: infer R
-        ]
-          ? [
-            h: MapFn<I, O1>,
-            h2: MapFn<O1, any>,
-            ...rest: any[]
-          ]
-          : [
-            h: MapFn<any, any>,
-            ...rest: any[]
-          ]
-    )
+// Error case
+// This should be given a compose tuple where the error is in the first 2 arguments.
+// If the first argument is invalid (i.e. not a unary function), then this
+// will return a tuple with a unary function as head.
+// If the first argument is valid, then we assume the second is invalid, and will
+// return a tuple with valid first 2 arguments.
+// E.g.:
+// ComposeError<[3]> = [any => any];
+// ComposeError<[I => O1, I2 => O2]> = [I => O1, O1 => any];
+type ComposeError<T extends any[]> =( 
+  T extends ComposeTuple1<infer I, infer O1, infer R>
+    ? ComposeTuple2<I, O1, any, any[]> 
+    : ComposeTuple1<any, any, any[]>
+)
 
     /**
      * Compose guard. When valid, output is same as input. When not valid, output matches input up to error
      */
-type ComposeArgs<T extends any[]> =
+type ValidComposeArgs<T extends any[]> =
   T extends [MapFn<infer I, infer O>] // Base case: single function.
   ? T
   : (
     // Recursive case: test if the first 2 functions are valid
-    T extends ValidateComposeHead2<infer I, infer O1, infer O2, infer REST>
-      ? ValidateComposeTail<T, I, O1, O2, REST>
+    T extends ComposeTuple2<infer I, infer O1, infer O2, infer REST>
+      ? (
+        ComposeTuple1<O1, O2, REST> extends ValidComposeArgs<ComposeTuple1<O1, O2, REST>> 
+        // If it is, all good!
+          ? T 
+        // If recursive fails, then we propagate the "Error" type further (see below)
+        // TODO: this currently duplicates the ValidComposeArgs clause, we can maybe DRY this up
+          : [
+            h: MapFn<I, O1>,
+            ...rest: ValidComposeArgs<[MapFn<O1, O2>, ...REST]>
+          ]
+      )
       : ComposeError<T>
-  )
+    )
 
   // THIS IS THE ONE!!!
   // It rejects the proper thing
-function compose<T extends any[]>(...funcs: ComposeArgs<T>) {}
+function compose<T extends any[]>(...funcs: ValidComposeArgs<T>) {}
 
 compose((i: string) => 3);
 compose((i: string) => 3, (i: number) => i);
