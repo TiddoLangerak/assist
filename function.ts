ee1b9aa655e055fcf148c73bc1f1e7151a24bdf7
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-08 23:39:37 -0300

    Comments, todos, little bit of cleanup

--- function.ts
+++ function.ts
@@ -83,22 +83,30 @@ type ComposeFromArgs<T extends any[]> =
       : never
     );
 
+    /**
+     * Compose guard. When valid, output is same as input. When not valid, output matches input up to error
+     */
 type CG<T extends any[]> =
-  T extends [(i: infer I) => infer O]
+  T extends [(i: infer I) => infer O] // Base case: single function.
   ? T
   : (
+    // Recursive case: test if the first 2 functions are valid
     T extends [h1: (i: infer I) => infer IO, h2: (i: infer IO) => infer O, ...rest: infer R]
     ? (
-      [(i: IO) => O, ...R] extends CG<[(i: IO) => O, ...R]>
-        ? T
-        : [h: (i: IO) => O, h2: (i: O) => any, ...rest: any[]]
+      // If the first 2 are valid, then we recursively test if the second onwards is valid
+      [(i: IO) => O, ...R] extends CG<[(i: IO) => O, ...R]> 
+        // If it is, all good!
+        ? T 
+        // If recursive fails, then we propagate the "Error" type further (see below)
+        // TODO: this currently duplicates the CG clause, we can maybe DRY this up
+        : [h: (i: I) => IO, ...rest: CG<[(i: IO) => O, ...R]>]
     )
-    //: [h: (i: I) => IO, h2: (i: IO) => any, ...rest: any[]]
-    //: never
-    : ( // Error case
+      : ( // Error case
+         // If the first 2 don't match, then we construct a type that will fail on the second parameter (i.e. a type that forces a match), and any[] for the remainder
+         // We have some extra conditional here to capture the parameters again (given that the previous conditional was NOT a match)
        T extends [h: (i: infer I) => infer O, ...rest: infer R]
         ? [h: (i: I) => O, h2: (i: O) => any, ...rest: any[]]
-        : never
+        : never // TODO: first arg is not a unary function, we should construct an appropriate type
     )
   )
 
@@ -109,6 +117,8 @@ function cg4<T extends any[]>(...funcs: CG<T>) {}
 cg4((i: string) => 3);
 cg4((i: string) => 3, (i: number) => i);
 cg4((i: string) => 3, (i: string) => i);
+cg4((i: string) => 3, (i: number) => i, (i: number) => i, (i: number) => i);
+cg4((i: string) => 3, (i: number) => i, (i: number) => i, (i: number) => i, (i: string) => i, (i:string) => i);
 
 
 function composeFromArgs<T extends any[]>(...funcs: ComposeFromArgs<T>) {
