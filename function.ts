53efd5e8a6130c7feb84a7e5f6fe8d90e491b77d
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-09 10:54:15 -0300

    WIP more QOL improvements

--- function.ts
+++ function.ts
@@ -12,19 +12,6 @@ type ComposeTuple1<I, O, REST extends any[]> = [
   ...rest: REST
 ];
 
-// TODO: this currently triggers infinite recursion...
-type ValidateComposeTail<T extends any[], I, O1, O2, REST extends any[]> = (
-  ComposeTuple1<O1, O2, REST> extends ValidComposeArgs<ComposeTuple1<O1, O2, REST>> 
-    // If it is, all good!
-    ? T 
-    // If recursive fails, then we propagate the "Error" type further (see below)
-    // TODO: this currently duplicates the ValidComposeArgs clause, we can maybe DRY this up
-    : [
-      h: MapFn<I, O1>,
-      ...rest: ValidComposeArgs<[MapFn<O1, O2>, ...REST]>
-    ]
-);
-
 // Error case
 // This should be given a compose tuple where the error is in the first 2 arguments.
 // If the first argument is invalid (i.e. not a unary function), then this
@@ -40,26 +27,55 @@ type ComposeError<T extends any[]> =(
     : ComposeTuple1<any, any, any[]>
 )
 
-    /**
-     * Compose guard. When valid, output is same as input. When not valid, output matches input up to error
-     */
+// Util to force an infered argument to be an array
+type InferArray<T extends any[]> = T;
+
+// TODO: this currently triggers infinite recursion...
+type ValidateComposeTail<T extends any[], I, O1, O2, REST extends any[]> =(
+        // Capture the valid version
+        ValidComposeArgs<ComposeTuple1<O1, O2, REST>> extends InferArray<infer ValidatedTail>
+        ? (
+          // Then recursively validate the tail. 
+          // If the tail is valid, then ValidComposeArgs will return the tail itself.
+          // We use extends as an == sign here.
+          // I.e. we compare the tail with the validated version of the tail
+          ComposeTuple1<O1, O2, REST> extends ValidatedTail
+          // If the tail is validated, then we're good to go, and can return our input unchanged.
+          ? T
+          // If the tail is NOT valid, then we need to construct our validated version of the tail, recursively
+          : ComposeTuple1<I, O1, ValidatedTail>
+        )
+        : [never]
+      );
+
+
+/**
+ * Compose guard. When valid, output is same as input. When not valid, output matches input up to error
+ */
 type ValidComposeArgs<T extends any[]> =
   T extends [MapFn<infer I, infer O>] // Base case: single function.
   ? T
   : (
     // Recursive case: test if the first 2 functions are valid
     T extends ComposeTuple2<infer I, infer O1, infer O2, infer REST>
+      ? ValidateComposeTail<T, I, O1, O2, REST>
+    /*
       ? (
-        // Then recursively validate the tail. 
-        // If the tail is valid, then ValidComposeArgs will return the tail itself.
-        // We use extends as an == sign here.
-        // I.e. we compare the tail with the validated version of the tail
-        ComposeTuple1<O1, O2, REST> extends ValidComposeArgs<ComposeTuple1<O1, O2, REST>> 
+        // Capture the valid version
+        ValidComposeArgs<ComposeTuple1<O1, O2, REST>> extends InferArray<infer ValidatedTail>
+        ? (
+          // Then recursively validate the tail. 
+          // If the tail is valid, then ValidComposeArgs will return the tail itself.
+          // We use extends as an == sign here.
+          // I.e. we compare the tail with the validated version of the tail
+          ComposeTuple1<O1, O2, REST> extends ValidatedTail
           // If the tail is validated, then we're good to go, and can return our input unchanged.
-          ? T 
+          ? T
           // If the tail is NOT valid, then we need to construct our validated version of the tail, recursively
-          : ComposeTuple1<I, O1, ValidComposeArgs<ComposeTuple1<O1, O2, REST>>>
-      )
+          : ComposeTuple1<I, O1, ValidatedTail>
+        )
+        : [never]
+      )*/
       : ComposeError<T>
     )
 
