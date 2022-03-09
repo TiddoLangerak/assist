f3d2ecaa823ca601c1f8430edac70aff0c113ebd
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-08 21:47:23 -0300

    Actual rejection, but poor

--- function.ts
+++ function.ts
@@ -24,13 +24,15 @@ type Tuple2<T extends any[]> =
     : never
   );
 
+class INVALID_COMPOSE {};
+
 type Compose1<T extends any[]> =
   T extends [(i: infer I) => infer O]
   ? (i: I) => O
   : (
     T extends [head: (i: infer I) => infer O, ...rest: infer R]
     ? (i: I) => ReturnType<Compose2<O, R>>
-    : never
+    : (i: INVALID_COMPOSE) => INVALID_COMPOSE
   );
 
 type Compose2<I, T extends any[]> =
@@ -39,8 +41,19 @@ type Compose2<I, T extends any[]> =
   : (
     T extends [head: (i: I) => infer O, ...rest: infer R]
     ? (i: I) => ReturnType<Compose2<O, R>>
-    : never
+    : (i: INVALID_COMPOSE) => INVALID_COMPOSE
   );
+
+function compose<T extends any[]>(...funcs: T) : Compose1<T> {
+  return null as any;
+}
+
+const cc = compose((i: string) => i.length);
+const r: number = cc("foo");
+const cc2 = compose((i: string) => i.length, (n: number) => [n]);
+const r2: number[] = cc2("fo");
+const ic = compose((i: string) => i.length, (i: string) => i.length);
+const r3 : string = ic("foo");
 
 const c : Compose1<[(i: string) => number]> = (i: string) => i.length;
 const c2: Compose1<[(i: string) => number, (i: number) => [number]]> = (i: string) => [i.length];
