b1c21a50522e274a0b925d1ff62888157a938386
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-09 10:03:39 -0300

    Use MapFn instead of arrows for legibility

--- function.ts
+++ function.ts
@@ -1,48 +1,47 @@
 export type MapFn<In, Out> = (input: In) => Out;
 
-
     /**
      * Compose guard. When valid, output is same as input. When not valid, output matches input up to error
      */
 type ComposeArgs<T extends any[]> =
-  T extends [(i: infer I) => infer O] // Base case: single function.
+  T extends [MapFn<infer I, infer O>] // Base case: single function.
   ? T
   : (
     // Recursive case: test if the first 2 functions are valid
     T extends [
-      h1: (i: infer I) => infer O1,
-      h2: (i: infer O1) => infer O2,
+      h1: MapFn<infer I, infer O1>,
+      h2: MapFn<infer O1, infer O2>,
       ...rest: infer REST
     ]
       ? (
         // If the first 2 are valid, then we recursively test if the second onwards is valid
         [
-          h: (i: O1) => O2,
+          h: MapFn<O1, O2>,
           ...rest: REST
-        ] extends ComposeArgs<[(i: O1) => O2, ...REST]> 
+        ] extends ComposeArgs<[MapFn<O1, O2>, ...REST]> 
           // If it is, all good!
           ? T 
           // If recursive fails, then we propagate the "Error" type further (see below)
           // TODO: this currently duplicates the ComposeArgs clause, we can maybe DRY this up
           : [
-            h: (i: I) => O1,
-            ...rest: ComposeArgs<[(i: O1) => O2, ...REST]>
+            h: MapFn<I, O1>,
+            ...rest: ComposeArgs<[MapFn<O1, O2>, ...REST]>
           ]
       )
       : ( // Error case
          // If the first 2 don't match, then we construct a type that will fail on the second parameter (i.e. a type that forces a match), and any[] for the remainder
          // We have some extra conditional here to capture the parameters again (given that the previous conditional was NOT a match)
         T extends [
-          h: (i: infer I) => infer O1,
+          h: MapFn<infer I, infer O1>,
           ...rest: infer R
         ]
           ? [
-            h: (i: I) => O1,
-            h2: (i: O1) => any,
+            h: MapFn<I, O1>,
+            h2: MapFn<O1, any>,
             ...rest: any[]
           ]
           : [
-            h: (i: any) => any,
+            h: MapFn<any, any>,
             ...rest: any[]
           ]
     )
