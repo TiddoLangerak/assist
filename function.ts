4dda0174a86f0b0571fc097f32fe4b2310055312
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-09 09:56:09 -0300

    Improve naming

--- function.ts
+++ function.ts
@@ -9,21 +9,21 @@ type ComposeArgs<T extends any[]> =
   ? T
   : (
     // Recursive case: test if the first 2 functions are valid
-    T extends [h1: (i: infer I) => infer IO, h2: (i: infer IO) => infer O, ...rest: infer R]
+    T extends [h1: (i: infer I) => infer O1, h2: (i: infer O1) => infer O2, ...rest: infer REST]
     ? (
       // If the first 2 are valid, then we recursively test if the second onwards is valid
-      [(i: IO) => O, ...R] extends ComposeArgs<[(i: IO) => O, ...R]> 
+      [(i: O1) => O2, ...REST] extends ComposeArgs<[(i: O1) => O2, ...REST]> 
         // If it is, all good!
         ? T 
         // If recursive fails, then we propagate the "Error" type further (see below)
         // TODO: this currently duplicates the ComposeArgs clause, we can maybe DRY this up
-        : [h: (i: I) => IO, ...rest: ComposeArgs<[(i: IO) => O, ...R]>]
+        : [h: (i: I) => O1, ...rest: ComposeArgs<[(i: O1) => O2, ...REST]>]
     )
       : ( // Error case
          // If the first 2 don't match, then we construct a type that will fail on the second parameter (i.e. a type that forces a match), and any[] for the remainder
          // We have some extra conditional here to capture the parameters again (given that the previous conditional was NOT a match)
-       T extends [h: (i: infer I) => infer O, ...rest: infer R]
-        ? [h: (i: I) => O, h2: (i: O) => any, ...rest: any[]]
+       T extends [h: (i: infer I) => infer O1, ...rest: infer R]
+        ? [h: (i: I) => O1, h2: (i: O1) => any, ...rest: any[]]
         : [h: (i: any) => any, ...rest: any[]]
     )
   )
