941bd1d746260fc8caa46e089eba2b6a297ed256
Tiddo Langerak (Termux Tablet) <tiddolangerak+tablettermux@gmail.com>
2022-03-11 18:43:09 -0300

    Add explicit input type to compose

--- function.ts
+++ function.ts
@@ -2,8 +2,8 @@ import { FunctionChain, ChainResult, chain } from './function/chain';
 import { ExpectType } from './test';
 
 export type Fn<In, Out> = (input: In) => Out;
-export function pipe<I, F extends readonly any[]>(input: I, ...funcs: FunctionChain<F>): ReturnType<ChainResult<F>> {
-  const c : ChainResult<F> = chain(...funcs);
+export function pipe<I, F extends readonly any[]>(input: I, ...funcs: FunctionChain<I, F>): ReturnType<ChainResult<I, F>> {
+  const c : ChainResult<I, F> = chain(...funcs);
   return c(input);
   //return c(input);
 }
